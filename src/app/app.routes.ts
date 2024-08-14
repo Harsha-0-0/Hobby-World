ef2440f1d6ad39c3c8630eef888921fe46374ff4
Harsha <harshiismyname@gmail.com>
2024-08-14 22:19:15 +1000

    Initial setup of website

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component'; 
+import { HobbiesComponent } from './features/hobbies/hobbies.component';
+import { ContactComponent } from './features/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'hobbies', component: HobbiesComponent },
+    { path: 'contact', component: ContactComponent }
+];
